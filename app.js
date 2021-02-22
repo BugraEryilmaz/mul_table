580e7fe979d4b20c79b1289c8772594891b82f25
Buğra Eryılmaz <bugra.eryilmaz@metu.edu.tr>
2021-02-23 01:40:22 +0300

    testing

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 var path = require('path');
-const port = 3000
+const PORT_NUMBER = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     console.log("hello w");
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT_NUMBER, () => {
+    console.log(`Example app listening at http://localhost:${PORT_NUMBER}`)
 })
