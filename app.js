8aa41673a573e542291be1e5f7a421fd1e9c64a1
Buğra Eryılmaz <bugra.eryilmaz@metu.edu.tr>
2021-02-23 01:32:31 +0300

    nodejs first

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+var path = require('path');
+const port = 3000
+
+app.get('/', (req, res) => {
+    console.log("hello");
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
