6b7883b900c635083f96184e8bd853a1cb87af25
Buğra Eryılmaz <bugra.eryilmaz@metu.edu.tr>
2021-02-25 14:07:03 +0300

    leaderboard

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 var path = require('path');
 const PORT_NUMBER = process.env.PORT || 3000;
 
+const monk = require('monk');
+const db = monk(process.env.MONGODB_URI);
+const ips = db.get('scoreboard');
+
 app.use(express.static('public'));
+app.use(express.json());
 
 app.listen(PORT_NUMBER, () => {
     console.log(`Example app listening at http://localhost:${PORT_NUMBER}`)
+})
+
+app.post('/api/highscoreget', async (req, res) => {
+    const options = req.body.options;
+    const ret = await ips.find({'options': options}, { sort: { score: -1 }, limit: 5 });
+    res.send(ret);
+})
+
+app.post('/api/highscore', async (req, res) => {
+    const score = req.body.score;
+    const name = req.body.name.replace(/\s/g, "").toLowerCase();
+    const options = req.body.options;
+    const ret = await ips.find({'options': options}, { sort: { score: 1 }, limit: 5 });
+    var update = false;
+    ret.forEach(async (element) => {
+        if (element.name == name) {
+            if (score > element.score) {
+                ips.findOneAndUpdate({'name': name, 'options': options, 'score': element.score}, { $set: { 'score': score} });
+            }
+            update = true;
+        }
+    });
+    if (!update) {
+        if (ret.length < 5) {
+            ips.insert({'name': name, 'options': options, 'score': score});
+        }
+        else if (ret[0].score < score) {
+            ips.findOneAndUpdate({'name': ret[0].name, 'options': ret[0].options, 'score': ret[0].score}, { $set: {'name': name, 'options': options, 'score': score} });
+        }
+    }
+    res.send(''+score);
 })
